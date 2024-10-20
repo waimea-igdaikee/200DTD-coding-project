f1b870d0af77e7faad5f64241269a70c836f5855
waimea-igdaikee <igdaikee@waimea.school.nz>
2024-10-21 10:56:04 +1300

    Big init updates

--- src/Main.kt
+++ src/Main.kt
@@ -15,7 +15,57 @@
 /**
  * Program entry point
  */
+val coins = 4
 fun main() {
-    println("Hello World!")
+    val board = initBoard(10, coins)
+    displayBoard(board)
 }
+
+
+fun initBoard(size: Int, coins: Int): Map<Int, Char> {
+    val board = mutableMapOf<Int, Char>()
+    repeat(size) { i -> board[i + 1] = '_' }
+
+    // Coin init - inits and shuffles a list from 1 to size then inserts coins in these spots
+    val coinPositions = mutableListOf<Int>()
+    repeat(size) { index -> coinPositions.add(index + 1) }
+    coinPositions.shuffle()
+    board[coinPositions[1]] = 'g'
+    repeat(coins - 1) { i -> board[coinPositions[i + 2]] = 'c' }
+    return(board)
+}
+
+fun displayBoard(board: Map<Int, Char>) {
+    print("┌")
+    repeat(coins) {print("─")}
+    println("┐")
+
+    print("│")
+    repeat(coins)
+}
+
+
+
+
+
+
+//    val board = mutableListOf<String>()
+//    repeat(size) {board.add("_")} // Add size amount of spaces
+//
+//    // Coin init
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
