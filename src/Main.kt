4bfb23f0d1727b9419dec555918b5906627a8049
waimea-igdaikee <168692114+waimea-igdaikee@users.noreply.github.com>
2024-10-21 12:18:38 +1300

    Proper coin init and pretty table

--- src/Main.kt
+++ src/Main.kt
@@ -11,61 +11,44 @@
  * ------------------------------------------------------------------------
  */
 
-
-/**
- * Program entry point
- */
-val coins = 4
+val coins = 5
+val size = 12
 fun main() {
-    val board = initBoard(10, coins)
+    val board = initBoard(size, coins)
     displayBoard(board)
 }
 
 
 fun initBoard(size: Int, coins: Int): Map<Int, Char> {
     val board = mutableMapOf<Int, Char>()
-    repeat(size) { i -> board[i + 1] = '_' }
+    repeat(size) { i -> board[i] = '_' }
 
     // Coin init - inits and shuffles a list from 1 to size then inserts coins in these spots
     val coinPositions = mutableListOf<Int>()
-    repeat(size) { index -> coinPositions.add(index + 1) }
+    repeat(size-1) { index -> coinPositions.add(index + 1) }
     coinPositions.shuffle()
-    board[coinPositions[1]] = 'g'
-    repeat(coins - 1) { i -> board[coinPositions[i + 2]] = 'c' }
+    board[coinPositions[0]] = 'g'
+    repeat(coins - 1) { i -> board[coinPositions[i + 1]] = 'c' }
     return(board)
 }
 
 fun displayBoard(board: Map<Int, Char>) {
-    print("┌")
-    repeat(coins) {print("─")}
-    println("┐")
+    print(" ")
+    for (i in 1..size) {
+        if (i < 10) {print(" ${i}  ")}
+        else {print("${i}  ")}
+    }
+    println()
+    
+    print("┌─")
+    repeat(size-1) {print("──┬─")}
+    println("──┐")
 
     print("│")
-    repeat(coins)
+    repeat(size) { i -> print(" ${board[i]} │")}
+    println()
+    
+    print("└─")
+    repeat(size-1) {print("──┴─")}
+    println("──┘")
 }
-
-
-
-
-
-
-//    val board = mutableListOf<String>()
-//    repeat(size) {board.add("_")} // Add size amount of spaces
-//
-//    // Coin init
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
